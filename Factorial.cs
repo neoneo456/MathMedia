33d1b1908c7bbc5b2557758bbb9785c423ed4940
neoneo456 <61343411+neoneo456@users.noreply.github.com>
2020-11-21 13:32:24 +0700

    Add files via upload

---
+++ Factorial.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, fact = 1, number;
+            Console.Write("Number: ");
+            number = int.Parse(Console.ReadLine());
+            for (i = 1; i <= number; i++)
+            {
+                fact = fact * i;
+            }
+            Console.Write("Factorial of " + number + " is: " + fact);
+            Console.ReadLine();
+        }
+    }
+}
